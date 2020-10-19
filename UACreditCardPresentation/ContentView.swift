9a2d9170fb0c047bced84de79cfc06fd267e1b0e
Edgar <ee1206@hotmail.com>
2020-10-19 10:25:34 -0500

    Initial Commit

---
+++ UACreditCardPresentation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UACreditCardPresentation
+//
+//  Created by Edgar Escorza on 19/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UACreditCardPresentation/UACreditCardPresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UACreditCardPresentationApp.swift
+//  UACreditCardPresentation
+//
+//  Created by Edgar Escorza on 19/10/20.
+//
+
+import SwiftUI
+
+@main
+struct UACreditCardPresentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
