69bfe32d2691527ebc4b31732ae71878b082a02c
Edgar <ee1206@hotmail.com>
2020-10-19 11:22:33 -0500

    Create interface builder to show the front and the back of the credit card

--- UACreditCardPresentation/CreditCard.swift
+++ UACreditCardPresentation/CreditCard.swift
@@ -7,9 +7,12 @@
 
 import SwiftUI
 
-struct CreditCard: View {
+struct CreditCard<Content>: View where Content: View {
+    
+    var content: () -> Content
+    
     var body: some View {
-        CreditCardFront()
+        content()
     }
 }
 
@@ -18,12 +21,23 @@ struct CreditCardFront : View {
         VStack {
             Text("FRONT").foregroundColor(Color.white)
         }.frame(width: 300, height: 200)
-        .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.green]), startPoint : .leading, endPoint: : trailing))
+        .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.green]), startPoint: .leading, endPoint: .trailing))
+        .cornerRadius(10)
+    }
+}
+
+struct CreditCardBack : View {
+    var body: some View {
+        VStack {
+            Text("BACK").foregroundColor(Color.white)
+        }.frame(width: 300, height: 200)
+        .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.green]), startPoint: .leading, endPoint: .trailing))
+        .cornerRadius(10)
     }
 }
 
 struct CreditCard_Previews: PreviewProvider {
     static var previews: some View {
-        CreditCard()
+        CreditCard<CreditCardFront>(content: { CreditCardFront() })
     }
 }
